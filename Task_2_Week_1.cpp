b1cf264c9a0ec4b4fca9b17842de163a6ce39dd3
AbdulRehman4317 <166744334+AbdulRehman4317@users.noreply.github.com>
2025-01-31 23:08:48 +0500

    Add files via upload

---
+++ Task_2_Week_1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(void){
+	string name;
+	cout<<"Enter your name: ";
+	getline(cin, name);
+	cout<<"Hello "<<name<<"!"<<endl;
+	return 0;
+}
